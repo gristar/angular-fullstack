20fdc000ae3d1bd060c7be54e0f09054a9490c8b
chensheng <1129380093@qq.com>
2016-10-13 09:47:24 +0800

    tijiao

--- angular-pigy/express.js
+++ angular-pigy/express.js
@@ -1,21 +1,33 @@
 var express = require('express');
 var app = express();
 
+app.get("/", function(req, res) {
+	res.redirect("/pigbank/detail");
+})
+
+app.get("/pigbank", function(req, res) {
+	res.redirect("/pigbank/detail");
+})
+
+app.get('/test', function(req, res) {
+	res.send('test');
+})
+
+app.get('/download', function(req, res) {
+	res.download("bower.json", "测试文件");
+})
+
 app.use(express.static('app'));
 
-app.get("/", function(req, res){
-	
+app.get("*", function(req, res) {
+	res.status(404).redirect('/error/404.html');
 })
 
-app.get('/test', function (req, res) {
-   res.send('test');
-})
+var server = app.listen(8088, function() {
 
-var server = app.listen(8088, function () {
+	var host = server.address().address
+	var port = server.address().port
 
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("应用实例，访问地址为 http://%s:%s", host, port)
+	console.log("应用实例，访问地址为 http://%s:%s", host, port)
 
 })
